9e1bf4608ac2b18e0f4b22d5e061e80104b70310
aerodiduch <fperezdiduch@gmail.com>
2022-12-07 02:53:21 -0300

    docs

--- ipsep.py
+++ ipsep.py
@@ -20,18 +20,45 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def read_file_data(file):
+def read_file_data(file: str) -> list:
+    '''Reads data from the specific file
+
+    Args:
+        file (str): Filename to read from
+
+    Returns:
+        list: A list containing the ips on the file.
+    '''
     ip_list = []
     with open(file, 'r') as fh:
         for line in fh:
             ip_list.append(line.replace('\n', ''))
     return ip_list
 
-def identify_private(ips):
+def identify_private(ips: list) -> list:
+    '''Receives a list of ips and identifies which are private.
+
+    Args:
+        ips (list): List of ips
+
+    Returns:
+        list: A list containing only private ips.
+    '''
+    
     filtered_ips = list(filter(lambda v: re.match(re_pattern, v), ips))
     return filtered_ips
 
-def prepare_data(raw_ips, filtered_ips):
+def prepare_data(raw_ips: list, filtered_ips: list) -> str:
+    '''Preparing the data for its export/printing.
+
+    Args:
+        raw_ips (list): A list containing all of the ips
+        filtered_ips (list): A list containing only the private ips
+
+    Returns:
+        str: Text body ready to be printed/exported to a file
+    '''
+    
     public_ips = list(set(raw_ips).difference(filtered_ips))
     body = ''
     body += "*"*5 + 'PRIVATE IPS' + '*' *5 + '\n'
@@ -40,7 +67,14 @@ def prepare_data(raw_ips, filtered_ips):
     body += "\n".join([i for i in public_ips])
     return body
 
-def dump_data(data, output_file):
+def dump_data(data: str, output_file: str):
+    '''Dumps data to a file/stdout  
+
+    Args:
+        data (str): Data to be exported/printed 
+        output_file (str): Optional. Path of the file to be writed.
+    '''
+    
     if output_file is None:
         print(data)
     else:
